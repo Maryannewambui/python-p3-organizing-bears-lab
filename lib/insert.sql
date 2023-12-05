a986ea021bbb896bbf4fa6cc7610df2ae873d8b2
Maryannewambui <trimbleleewambui@gmail.com>
2023-12-05 15:49:49 +0300

    assignment complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Black', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Brown', 'aggressive', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'F', 'Black', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'white', 'serious', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Black', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Brown', 'sociable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'Black', 'attention-seeker', 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'white', 'unhappy', 0);
